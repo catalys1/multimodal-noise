6f823b0811962e1fcfb3f358f2aa1f638b7c6e72
Connor Anderson <connor.anderson@byu.edu>
2022-08-18 14:24:14 -0400

    Add GatherLayer

--- mmnoise/utils/distributed.py
+++ mmnoise/utils/distributed.py
@@ -15,3 +15,20 @@ def rank():
     if is_distributed():
         return torch.distributed.get_rank()
     return 0
+
+
+class GatherLayer(torch.autograd.Function):
+    """Gather tensors from all process, supporting backward propagation."""
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        output = [torch.zeros_like(input) for _ in range(torch.distributed.get_world_size())]
+        torch.distributed.all_gather(output, input)
+        return tuple(output)
+
+    @staticmethod
+    def backward(ctx, *grads):
+        (input,) = ctx.saved_tensors
+        grad_out = torch.zeros_like(input)
+        grad_out[:] = grads[torch.distributed.get_rank()]
+        return grad_out
