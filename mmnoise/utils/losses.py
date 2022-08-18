61af3895e15d0921bc750f8519cd664671c8a70c
Connor Anderson <connor.anderson@byu.edu>
2022-08-18 14:24:58 -0400

    Add reformulated loss function

--- mmnoise/utils/losses.py
+++ mmnoise/utils/losses.py
@@ -29,3 +29,22 @@ class CLIPLoss(torch.nn.Module):
     def forward(self, x, y, temperature_scale=1.0):
         loss = 0.5 * (self.contrastive_loss(x, y, temperature_scale) + self.contrastive_loss(y, x, temperature_scale))
         return loss
+
+
+class CLIPLossFullBidirectional(torch.nn.Module):
+    def contrastive_loss(self, x, y, temperature_scale):
+        labels = torch.arange(x.shape[0], device=x.device)
+        logits = x @ y.T
+        if isinstance(temperature_scale, torch.nn.Module):
+            logits = temperature_scale(logits)
+        else:
+            logits = logits * (1 / temperature_scale)
+        
+        return torch.nn.functional.cross_entropy(logits, labels)
+
+    def forward(self, x, y, temperature_scale=1.0):
+        if distributed.is_distributed():
+            x = torch.cat(distributed.GatherLayer.apply(x), 0)
+            y = torch.cat(distributed.GatherLayer.apply(y), 0)
+        loss = 0.5 * (self.contrastive_loss(x, y, temperature_scale) + self.contrastive_loss(y, x, temperature_scale))
+        return loss
